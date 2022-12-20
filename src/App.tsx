4cdc90ad16b723b41327e7ba323e9d9cb90d9d08
jatin510 <jatin6972@gmail.com>
2022-12-20 10:24:43 +0530

    added react-router-dom. And initial setup for react routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Link, NavLink, Route, Routes } from 'react-router-dom';
 
-function App() {
-  const [count, setCount] = useState(0)
+import Home from './components/Home';
+import PageNotFound from './components/PageNotFound';
 
+function Navbar() {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <>
+      <h1>Navbar</h1>
+
+      <ul>
+        <li>
+          <NavLink to="/">Home </NavLink>
+        </li>
+
+        <li>
+          <NavLink to="/notfound">PageNotFound </NavLink>
+        </li>
+      </ul>
+    </>
+  );
 }
 
-export default App
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/PageNotFound/PageNotFound.tsx
@@ -0,0 +1,5 @@
+import './PageNotFound.css';
+
+export default function PageNotFound() {
+  return <div>PageNotFound</div>;
+}
