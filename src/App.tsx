df64c638414d46f58155687da8ede1628df38d64
rakeshrengaraj <rakeshrengaraj@bitbucket.org>
2023-01-04 18:41:03 +0530

    Added sentry to the project to track issues

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,21 @@ import { Layout } from "antd";
 const { Header, Footer,Content } = Layout;
 
 
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+
+
+Sentry.init({
+  dsn: "https://01fd072da8d244c7b06ce8ba92f2181c@o4504436238385152.ingest.sentry.io/4504440908283904",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
 const App: React.FC = () => (
   <Layout>
       <Header>
